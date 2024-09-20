0c94c0018439eb09a22364523bca79cf5705f11c
JUNHAN ZHANG <144378024+Clearsky21z@users.noreply.github.com>
2024-09-20 14:59:43 -0400

    Downloaded actual Toronto Island ferry ticket data and saved it as 'unedited_ferry_ticket_counts.csv'.

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,36 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from the Toronto Island Ferry Ticket Counts dataset
+# Author: John Zhang
+# Date: September 2024
+# Contact: junhan.zhang@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
+# Any other information needed? None
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# Get package information for Toronto Island Ferry Ticket Counts
+package <- show_package("toronto-island-ferry-ticket-counts")
 
+# Get all resources for this package
+resources <- list_package_resources("toronto-island-ferry-ticket-counts")
 
+# Identify datastore resources; by default, Open Data Toronto sets datastore resources to CSV or GeoJSON
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# Load the first datastore resource (CSV format)
+the_raw_data <- filter(datastore_resources, row_number() == 1) %>% get_resource()
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+# Save the downloaded data to the raw_data folder
+write_csv(the_raw_data, "data/raw_data/unedited_ferry_ticket_counts.csv")
+
+# Print message to confirm successful download and saving
+print("Data successfully downloaded and saved as 'unedited_ferry_ticket_counts.csv'")
+
 
          
