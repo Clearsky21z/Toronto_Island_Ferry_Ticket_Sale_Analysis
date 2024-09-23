6b26421877beb78479c932becbe3ddc3fe62a778
JUNHAN ZHANG <144378024+Clearsky21z@users.noreply.github.com>
2024-09-23 18:20:05 -0400

    Added script for cleaning data and the output file

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,36 +9,23 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(lubridate)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+# Load the raw data
+raw_data <- read_csv("data/raw_data/unedited_ferry_ticket_counts.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+# Clean the dataset
+cleaned_data <- raw_data |>
+  janitor::clean_names() |>  # Clean column names
+  select(x_id, timestamp, redemption_count, sales_count) |>  # Select relevant columns based on correct names
+  mutate(timestamp = as_datetime(timestamp)) |>  # Convert timestamp to datetime format
+  filter(year(timestamp) >= 2016 & year(timestamp) <= 2023) |>  # Filter rows between 2016-2023
+  mutate(total_sales = redemption_count + sales_count) |>  # Create new column for total sales
+  drop_na()  # Drop rows with missing values
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+#### Save cleaned data ####
+write_csv(cleaned_data, "data/analysis_data/cleaned_ferry_ticket_counts.csv")
+
+# Print message to confirm cleaning process
+print("Data successfully cleaned and saved as 'cleaned_ferry_ticket_counts.csv'")
